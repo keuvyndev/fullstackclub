d1e77905c567ccd8532c1a67c3c274657b485a45
keuvyntdev <keuvyntdev@gmail.com>
2025-01-07 08:35:53 -0300

    feat: add route to delete task

--- fsc-taskmanager-backend/index.js
+++ fsc-taskmanager-backend/index.js
@@ -18,12 +18,15 @@ connectToDatabse();
 app.get("/tasks", async (req, res) => {
 
    try {
+
       const tasks = await TaskModel.find({});
       res.status(200).send(tasks);
-      //console.log("Http return 200!");
+
    } catch (error) {
+
       res.status(500).send(error.message);
       //console.error(error);
+
    }
 
 });
@@ -32,12 +35,42 @@ app.get("/tasks", async (req, res) => {
 // Função: Criar uma nova task
 app.post("/tasks", async (requisicao, resultado) => {
    try {
+
       const newTask = new TaskModel(requisicao.body);
       await newTask.save();
       resultado.status(201).send(newTask) // HTTP 201: Criação realizada com sucesso!
+
    } catch (error) {
-      res.status(500).send(error.message)
+
+      resultado.status(500).send(error.message)
+
    }
+});
+
+// Efetua requisição DELETE e exporta na rota 'localhost:8000/tasks' com 'id' dinâmico
+// Função: Deleta uma nova task
+app.delete("/tasks/:id", async (requisicao, resultado) => {
+
+   try {
+
+      const taskId = requisicao.params.id
+
+      const taskToDelete = await TaskModel.findById(taskId);
+
+      // Trata retorno caso ID não exista
+      if (!taskToDelete) {
+         return resultado.status(500).send('Esta tarefa não foi encontrada!')
+      }
+
+      const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+      resultado.status(200).send(deletedTask)
+
+   } catch (error) {
+
+      resultado.status(500).send(error.message)
+
+   }
+
 });
 
 app.listen(8000, () => console.log("Listening on port 8000!"));
