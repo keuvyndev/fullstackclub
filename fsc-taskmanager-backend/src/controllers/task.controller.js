021610e93b9285ea70c4b95d030144a5b6f298bb
keuvyntdev <keuvyntdev@gmail.com>
2025-01-08 23:05:45 -0300

    feat: move post patch and get methods to controller

--- fsc-taskmanager-backend/src/controllers/task.controller.js
+++ fsc-taskmanager-backend/src/controllers/task.controller.js
@@ -12,17 +12,102 @@ class TaskController {
    // --- MÉTODOS DA CLASSE
 
    /* 
-   * Objetivo: Capturar todas as Tasks.
+   * Objetivo do método: Capturar todas as Tasks.
    * Descrição: Efetua requisição GET e retornando todas as tasks.
    */
 
-   async getTasks() {
+   async getAll() {
       try {
          const tasks = await TaskModel.find({});
-         res.status(200).send(tasks);
+         this.res.status(200).send(tasks);
       } catch (error) {
-         res.status(500).send(error.message);
+         this.res.status(500).send(error.message);
          //console.error(error);
+      }
+   }
+
+   /* 
+   * Objetivo do método: Captura uma task através da ID.
+   * Descrição: Efetua requisição do tipo GET e retorna apenas a task que corresponda com a 'id' caso exista.
+   */
+
+   async getById() {
+      try {
+         const taskId = (this.req.params.id);
+         const task = await TaskModel.findById(taskId);
+         if (!task) {
+            this.res.status(404).send("Task não encontrada!");
+         }
+         this.res.status(200).send(task);
+      } catch (error) {
+         this.res.status(500).send(error.message);
+      }
+   }
+
+
+   /* 
+   * Objetivo do método: Criar nova task.
+   * Descrição: Efetua requisição do tipo POST criando uma nova task apartir do body.
+   */
+
+   async post() {
+      try {
+         const newTask = new TaskModel(this.req.body);
+         await newTask.save();
+         this.res.status(201).send(newTask) // HTTP 201: Criação realizada com sucesso!
+      } catch (error) {
+         this.res.status(500).send(error.message);
+      }
+   }
+
+   /* 
+   * Objetivo do método: Atualizar o 'isComplete' de uma task.
+   * Descrição: Efetua requisição do tipo PATCH atribuindo o novo status a task através da ID.
+   */
+
+   async patch() {
+
+      try {
+         const taskParamId = this.req.params.id;
+         const taskData = this.req.body;
+         const taskToUpdate = await TaskModel.findById(taskParamId);
+
+         // Validando chaves de update do body
+         const allowedUpdate = ['isCompleted']
+         const requestedUpdates = Object.keys(taskData); // Retorna as chaves do JSON do body
+
+         for (keyToUpdate of requestedUpdates) {
+            if (allowedUpdate.includes(keyToUpdate)) {
+               taskToUpdate[keyToUpdate] = this.req.body[keyToUpdate]
+            } else {
+               return this.res.status(500).send(`${keyToUpdate} não pode ser alterado!`)
+            }
+         }
+         await taskToUpdate.save();
+         this.res.status(200).send(taskToUpdate)
+
+      } catch (error) {
+         this.res.status(500).send(error.message)
+      }
+   }
+
+   /* 
+   * Objetivo do método: Deletar uma task através da ID.
+   * Descrição: Efetua requisição do tipo DELETE deletando a task que corersponda a ID caso exista.
+   */
+
+   async delete() {
+      try {
+         const taskId = this.req.params.id;
+         const taskToDelete = await TaskModel.findById(taskId);
+         // Trata retorno caso ID não exista
+         if (!taskToDelete) {
+            return this.res.status(404).send('Esta tarefa não foi encontrada!') // HTTP 404: Não encontrado
+         }
+         const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+         this.res.status(200).send(deletedTask)
+      } catch (error) {
+         this.res.status(500).send(error.message)
       }
    }
 }
--- fsc-taskmanager-backend/src/routes/task.routes.js
+++ fsc-taskmanager-backend/src/routes/task.routes.js
@@ -6,83 +6,34 @@ const TaskModel = require('../models/task.model')
 // Definindo rota express
 const router = express.Router();
 
+// Efetua requisição GET e exporta na rota 'localhost:8000/tasks'
+// Função: Retorna todas as tasks
+router.get("/", async (req, res) => {
+   return new TaskController(req, res).getAll();
+});
+
 // Efetua requisição GET e exporta na rota 'localhost:8000/tasks' com 'id' dinâmico
 // Função: Retorna dados de uma task
 router.get("/:id", async (req, res) => {
-   return new TaskController(req, res).getTasks();
+   return new TaskController(req, res).getById();
 });
 
 // Efetua requisição POST e exporta na rota 'localhost:8000/tasks'
 // Função: Criar uma nova task
-router.post("/", async (requisicao, resultado) => {
-
-   try {
-
-      const newTask = new TaskModel(requisicao.body);
-      await newTask.save();
-      resultado.status(201).send(newTask) // HTTP 201: Criação realizada com sucesso!
-
-   } catch (error) {
-
-      resultado.status(500).send(error.message);
-
-   }
-
+router.post("/", async (req, res) => {
+   return new TaskController(req, res).post();
 });
 
 // Efetua requisição PATCH e exporta na rota 'localhost:8000/tasks' com 'id' dinâmico
 // Função: Retorna uma task atualizada
 router.patch("/:id", async (req, res) => {
-
-   try {
-
-      const taskParamId = req.params.id;
-      const taskData = req.body;
-      const taskToUpdate = await TaskMode.findById(taskParamId);
-
-      // Validando chaves de update do body
-      const allowedUpdate = ['isCompleted']
-      const requestedUpdates = Object.keys(taskData); // Retorna as chaves do JSON do body
-
-      for (keyToUpdate of requestedUpdates) {
-         if (allowedUpdate.includes(keyToUpdate)) {
-            taskToUpdate[keyToUpdate] = req.body[keyToUpdate]
-         } else {
-            return res.status(500).send(`${keyToUpdate} não pode ser alterado!`)
-         }
-      }
-      await taskToUpdate.save();
-      res.status(200).send(taskToUpdate)
-
-   } catch (error) {
-      res.status(500).send(error.message)
-   }
-
+   return new TaskController(req, res).patch();
 });
 
 // Efetua requisição DELETE e exporta na rota 'localhost:8000/tasks' com 'id' dinâmico
 // Função: Deleta uma nova task
-router.delete("/:id", async (requisicao, resultado) => {
-
-   try {
-
-      const taskId = requisicao.params.id;
-      const taskToDelete = await TaskModel.findById(taskId);
-
-      // Trata retorno caso ID não exista
-      if (!taskToDelete) {
-         return resultado.status(404).send('Esta tarefa não foi encontrada!') // HTTP 404: Não encontrado
-      }
-
-      const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-      resultado.status(200).send(deletedTask)
-
-   } catch (error) {
-
-      resultado.status(500).send(error.message)
-
-   }
-
+router.delete("/:id", async (req, res) => {
+   return new TaskController(req, res).delete();
 });
 
 module.exports = router;
