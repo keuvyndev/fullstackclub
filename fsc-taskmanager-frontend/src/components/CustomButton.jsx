023993ab1ba2a89dd1e6e96ce6f04d6b5281010b
keuvyntdev <keuvyntdev@gmail.com>
2025-01-18 16:50:38 -0300

    fix: add return to custom Button Component

--- fsc-taskmanager-frontend/src/components/CustomButton.jsx
+++ fsc-taskmanager-frontend/src/components/CustomButton.jsx
@@ -2,9 +2,11 @@ import "./CustomButtom.scss";
 
 // Children receberá o corpo do component CustomButton
 const CustomButton = ({ onClick, children }) => {
-    <div className="custom-button-container" onClick={onClick}>
-        {children}
-    </div>;
+    return (
+        <div className="custom-button-container" onClick={onClick}>
+            {children}
+        </div>
+    );
 };
 
 export default CustomButton;
