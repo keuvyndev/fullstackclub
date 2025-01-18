6067754e4a932540bf3aee12ab0e46c71aba0e4f
keuvyntdev <keuvyntdev@gmail.com>
2025-01-18 17:56:04 -0300

    feat: add react-toastify setup

--- fsc-taskmanager-frontend/src/index.js
+++ fsc-taskmanager-frontend/src/index.js
@@ -5,11 +5,24 @@ import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
 import App from './App';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      position="bottom-center"
+      autoClose={2500}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
   </React.StrictMode>
 );
 
