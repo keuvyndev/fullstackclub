674b40a4e96c297d82cb6bd27b878f6ffc60e0fc
keuvyntdev <keuvyntdev@gmail.com>
2025-01-18 23:54:44 -0300

    feat: add navigation to signOutButton in side bar component

--- fsc-taskmanager-frontend/src/components/SideBar.jsx
+++ fsc-taskmanager-frontend/src/components/SideBar.jsx
@@ -1,15 +1,22 @@
 import "./SideBar.scss";
 import logo from "../assets/images/logo.png";
 import CustomButton from "./CustomButton";
+import { useNavigate } from "react-router-dom";
 
 const SideBar = () => {
+    const navigate = useNavigate();
+
+    const handleSignOutClick = () => {
+        navigate("/login");
+    };
+
     return (
         <div className="sidebar-container">
             <div className="logo">
                 <img src={logo} alt="Full Stuck Club" />
             </div>
             <div className="sign-out">
-                <CustomButton>Sair</CustomButton>
+                <CustomButton onClick={handleSignOutClick}>Sair</CustomButton>
             </div>
         </div>
     );
