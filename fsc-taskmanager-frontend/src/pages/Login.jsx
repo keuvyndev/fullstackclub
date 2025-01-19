e2e3f240026abda4ceb35fd57cf8dce6729cf2db
keuvyntdev <keuvyntdev@gmail.com>
2025-01-18 23:44:45 -0300

    feat: add Login component

---
+++ fsc-taskmanager-frontend/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import "./login.scss";
+import logo from "../assets/images/logo-2.png";
+import CustomButton from "../components/CustomButton";
+
+const Login = () => {
+    return (
+        <div className="login-container">
+            <img src={logo} alt="Full Stack Club" />
+            <div className="button-container">
+                <CustomButton>Entrar</CustomButton>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
